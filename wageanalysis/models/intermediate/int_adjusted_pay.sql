277da7dc7206e7f26352740d9063d5ce757ea616
Gustavo Santos <ghuida@gmail.com>
2024-05-05 21:59:10 +0100

    Repo working with lighdash locally

--- wageanalysis/models/intermediate/int_adjusted_pay.sql
+++ wageanalysis/models/intermediate/int_adjusted_pay.sql
@@ -1,4 +1,11 @@
-with lattest as (
+{{
+  config(
+    materialized='view',
+    tags=['lightdash']
+  )
+}}
+
+with lattest_country_job_info as (
 select 
 	country,
 	job, 
@@ -14,36 +21,51 @@ select
 	base_pay_max, 
 	additional_pay_min, 
 	additional_pay_max,
-	row_number() over (partition by country, job order by last_update desc) as "rank"
+	row_number() over (partition by country, job order by last_update desc) as job_rank
 from {{ref('stg_country_job_info')}} scji),
+
+exchange_rate_rank as (
+select
+	ce.currency,
+	ce.exchange_rate,
+	row_number() over (partition by currency order by insert_date desc) as currency_rank
+from {{source('raw','currency_exchange')}} ce
+),
+
+lattest_exchange as (
+select 
+	*
+from exchange_rate_rank
+where currency_rank = 1
+),
 
 joined as (
 select 
-	l.country,
-	l.job, 
-	l.currency, 
-	l.pay, 
-	l."period", 
-	l.last_update, 
-	l.average_salary,
-	l.wage_description, 
-	l.pay_range_min, 
-	l.pay_range_max, 
-	l.base_pay_min, 
-	l.base_pay_max, 
-	l.additional_pay_min, 
-	l.additional_pay_max,
+	lcji.country,
+	lcji.job, 
+	lcji.currency, 
+	lcji.pay, 
+	lcji."period", 
+	lcji.last_update, 
+	lcji.average_salary,
+	lcji.wage_description, 
+	lcji.pay_range_min, 
+	lcji.pay_range_max, 
+	lcji.base_pay_min, 
+	lcji.base_pay_max, 
+	lcji.additional_pay_min, 
+	lcji.additional_pay_max,
 	c."name",
 	c.currency_name,
 	c.currency_code,
-	ce.exchange_rate 
-from lattest l
+	le.exchange_rate 
+from lattest_country_job_info lcji
 join {{source('raw','countrys')}} c 
-on lower(l.country) = lower(c."name")
-join {{source('raw','currency_exchange')}} ce 
-on c.currency_code = ce.currency 
-where "rank" = 1
-and concat_ws('-', l.country, l.currency, c.currency_code) 
+on lower(lcji.country) = lower(c."name")
+join lattest_exchange le 
+on c.currency_code = le.currency 
+where job_rank = 1
+and concat_ws('-', lcji.country, lcji.currency, c.currency_code) 
 	not in (select 
 				concat_ws('-', country, currency, currency_code) 
 			from {{ref('adt_currency_country')}} acc))
@@ -53,36 +75,36 @@ select
 	job, 
 	currency,
 	case
-		when "period" = 'mo' then ((pay / exchange_rate) * 12)::numeric::money
-		else (pay / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((pay / exchange_rate) * 12)::numeric
+		else (pay / exchange_rate)::numeric
 	end	as pay,
 	case
-		when "period" = 'mo' then ((average_salary / exchange_rate) * 12)::numeric::money
-		else (average_salary / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((average_salary / exchange_rate) * 12)::numeric
+		else (average_salary / exchange_rate)::numeric
 	end	as average_salary,
 	case
-		when "period" = 'mo' then ((pay_range_min / exchange_rate) * 12)::numeric::money
-		else (pay_range_min / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((pay_range_min / exchange_rate) * 12)::numeric
+		else (pay_range_min / exchange_rate)::numeric
 	end	as pay_range_min, 
 	case
-		when "period" = 'mo' then ((pay_range_max / exchange_rate) * 12)::numeric::money
-		else (pay_range_max / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((pay_range_max / exchange_rate) * 12)::numeric
+		else (pay_range_max / exchange_rate)::numeric
 	end	as pay_range_max, 
 	case
-		when "period" = 'mo' then ((base_pay_min / exchange_rate) * 12)::numeric::money
-		else (base_pay_min / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((base_pay_min / exchange_rate) * 12)::numeric
+		else (base_pay_min / exchange_rate)::numeric
 	end	as base_pay_min,
 	case
-		when "period" = 'mo' then ((base_pay_max / exchange_rate) * 12)::numeric::money
-		else (base_pay_max / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((base_pay_max / exchange_rate) * 12)::numeric
+		else (base_pay_max / exchange_rate)::numeric
 	end	as base_pay_max,
 	case
-		when "period" = 'mo' then ((additional_pay_min / exchange_rate) * 12)::numeric::money
-		else (additional_pay_min / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((additional_pay_min / exchange_rate) * 12)::numeric
+		else (additional_pay_min / exchange_rate)::numeric
 	end	as additional_pay_min,
 	case
-		when "period" = 'mo' then ((additional_pay_max / exchange_rate) * 12)::numeric::money
-		else (additional_pay_max / exchange_rate)::numeric::money
+		when "period" = 'mo' then ((additional_pay_max / exchange_rate) * 12)::numeric
+		else (additional_pay_max / exchange_rate)::numeric
 	end	as additional_pay_max,
 	"period" as base_period, 
 	last_update
